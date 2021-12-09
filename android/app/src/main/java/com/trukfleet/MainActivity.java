8b46cf8f087d67b8127c599c83f5a97be330e6da
Yagnik777 <yagnikchovatiya777@gmail.com>
2021-12-09 13:01:33 +0530

    Solve ndk error

---
+++ android/app/src/main/java/com/trukfleet/MainActivity.java
@@ -0,0 +1,10 @@
+package com.trukfleet;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
